6b5ec65d7aeb1b1c7bd30ebb4e3e9ab81344e062
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-02-04 17:58:45 -0300

    feat: add landing page component

---
+++ src/app/pages/landing/landing.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+// ng generate component <component>
+
+@Component({
+  selector: 'app-landing',
+  imports: [],
+  templateUrl: './landing.html',
+  styleUrl: './landing.sass',
+})
+export class Landing {}
