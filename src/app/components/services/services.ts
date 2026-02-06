a6c815f35d7ee68cc3a1cbaf1601f173e673b6a4
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-02-06 20:17:22 -0300

    feat: add services component

---
+++ src/app/components/services/services.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  imports: [],
+  templateUrl: './services.html',
+  styleUrl: './services.sass',
+})
+export class Services {
+
+}
--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Advantages } from '../../components/advantages/advantages';
 import { Header } from '../../components/header/header';
 import { Hero } from '../../components/hero/hero';
+import { Services } from '../../components/services/services';
 
 // ng generate component <component>
 
 @Component({
   selector: 'app-landing',
-  imports: [Header, Hero, Advantages],
+  imports: [Header, Hero, Advantages, Services],
   templateUrl: './landing.html',
   styleUrl: './landing.sass',
 })
