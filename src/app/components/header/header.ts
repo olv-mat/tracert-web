e62b4d8e6bbf8461a766f9d2da1fe1249c6a64a3
Matheus Oliveira <contamatheusprofissional@gmail.com>
2026-02-06 22:02:43 -0300

    feat: style header component

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.sass',
 })
-export class Header {}
+export class Header {
+  public menuOpen = false;
+
+  public toggleMenu(): void {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  public closeMenu(): void {
+    this.menuOpen = false;
+  }
+}
